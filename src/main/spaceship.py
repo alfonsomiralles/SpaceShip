f03381236107c09d951b5b2e62294dc9ec086f95
alfonsomiralles <alfonsomiralles1@gmail.com>
2022-06-10 10:27:22 +0200

    feature: All the Ships now have: total-power, power-not-in-use. Created tests

--- src/main/spaceship.py
+++ src/main/spaceship.py
@@ -2,20 +2,24 @@
 
 class Spaceship:
 
-    def __init__(self, name, health):
+    def __init__(self, name, health, totalpower, powernotinuse):
         if  health < 0:  
-            raise ValueError ("The Spaceship can't have health below 0. Please try again")       
+            raise ValueError ("The Spaceship can't have health below 0. Please try again") 
+        elif powernotinuse > totalpower:
+            raise ValueError ("power-not-in-use can't be higher than total-power")         
         else:
             self.name = name
             self.health = health
             self.alive = True
             self.weapon = 'weapon'
+            self.totalpower = totalpower
+            self.powernotinuse = powernotinuse
 
     def __hash__(self):
         return hash((self.name, self.health, self.alive))
 
     def __eq__(self, other):
-        return (self.name, self.health, self.alive, self.weapon) == (other.name, other.health, other.alive, other.weapon)
+        return (self.name, self.health, self.alive, self.weapon, self.totalpower, self.powernotinuse) == (other.name, other.health, other.alive, other.weapon, other.totalpower, other.powernotinuse)
 
     def __ne__(self, other):
         # Not strictly necessary, but to avoid having both x==y and x!=y
